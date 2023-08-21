69d4a32d42ba84ab63a407db98a459c176f262b5
palak saxena <saxenapalak345@gmail.com>
2023-08-21 12:14:37 +0530

    update

--- src/Components/RoleDice.js
+++ src/Components/RoleDice.js
@@ -1,10 +1,25 @@
 import styled from "styled-components";
+import dice_1 from "../images/dice_1.png";
+import dice_2 from "../images/dice_2.png";
+import dice_3 from "../images/dice_3.png";
+import dice_4 from "../images/dice_4.png";
+import dice_5 from "../images/dice_5.png";
+import dice_6 from "../images/dice_6.png";
 
 const RoleDice = ({ roleDice, currentDice }) => {
+  const diceImages = {
+    1: dice_1,
+    2: dice_2,
+    3: dice_3,
+    4: dice_4,
+    5: dice_5,
+    6: dice_6,
+  };
+  const diceImage = diceImages[currentDice];
   return (
     <DiceContainer>
       <div className="dice" onClick={roleDice}>
-        <img src={`/images/dice_${currentDice}.png`} alt="dice 1" />
+      <img src={diceImage} alt={`dice ${currentDice}`} />
       </div>
       <p>Click on Dice to roll</p>
     </DiceContainer>
--- src/Components/StartGame.js
+++ src/Components/StartGame.js
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 import { Button } from "../styled/Button";
+import images1 from "../images/dices.png"
+
 
 const StartGame = ({ toggle }) => {
   return (
     <Container>
       <div>
-        <img src="/images/dices.png"/>
+        <img src={images1} alt="dice"/>
       </div>
+
       <div className="content">
         <h1>Dice Game</h1>
         <Button onClick={toggle}>Play Now</Button>
